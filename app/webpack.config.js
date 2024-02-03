db14a688e5df32d09c6574c23899a7c96ca45fa1
船津壮平 <funatsusouhei@funatsusouheinoMacBook-Pro.local>
2024-02-04 06:20:44 +0900

    db production setting25

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -6,11 +6,12 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Turbolinks: 'turbolinks',
 }));
 
-module.exports = {
+// 'entry'を直接指定するのではなく、environment.config.mergeを使用してマージします
+environment.config.merge({
   entry: {
     main: 'app/javascript/application.js'
   },
   // 他の設定...
-};
+});
 
 module.exports = environment;
