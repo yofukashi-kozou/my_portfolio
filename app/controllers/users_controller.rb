5a403b1d3ba27f97c210a491e546cdcfc11f54db
船津壮平 <funatsusouhei@funatsusouheinoMacBook-Pro.local>
2024-04-23 05:54:50 +0900

    precomplete5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update] # ログイン済みユーザーのみアクセス可能なアクションを指定
+  before_action :correct_user, only: [:edit, :update] # 正しいユーザーのみアクセス可能なアクションを指定
+
   def show
     @user = User.find(params[:id])
 
@@ -58,8 +61,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :introduction,:avatar,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :introduction,:avatar,:password_confirmation)
     end
 
 
@@ -67,7 +69,7 @@ class UsersController < ApplicationController
     def logged_in_user
       unless logged_in?
         flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
+        redirect_to login_url, status: :forbidden
       end
     end
 
