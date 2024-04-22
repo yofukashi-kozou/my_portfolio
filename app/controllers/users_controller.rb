5c98379745271403f917150612dc8619bfbd0617
船津壮平 <funatsusouhei@funatsusouheinoMacBook-Pro.local>
2024-04-23 06:27:16 +0900

    precomplete6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -76,7 +76,7 @@ class UsersController < ApplicationController
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless current_user?(@user)
+      redirect_to(root_url, status: :see_other) unless @user == current_user
     end
 end
 
