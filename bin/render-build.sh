bdfe1d7f037b58c3a9e0ce9b8beebdb1c5d6dbe2
船津壮平 <funatsusouhei@funatsusouheinoMacBook-Pro.local>
2024-05-02 07:12:51 +0900

    error msg

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,4 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate # マイグレーションの実行
 # rails db:seed RAILS_ENV=production #本番環境のデータベースに初期データを投入
-# DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
